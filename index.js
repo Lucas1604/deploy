d7059149949a3b850930323503cdbde94e8b229c
lucas1604 <lucasdesignerlt@gmail.com>
2019-12-12 14:29:08 -0200

    llgggghgggl

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ app.get('/users', (req, res) =>{
     res.send('Hello Users!');
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log('Rodando com sucesso! :)')
 })
